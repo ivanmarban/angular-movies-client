ba3598c69caa3b4cc3f5bac4d02e915b203c0b62
Ivan Marban <ivan.marban@gmail.com>
2020-05-24 10:36:20 +0200

    Format code

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,100 +1,101 @@
 moviesApp.controller('HeaderCtrl', ['$scope', '$location',
-	function($scope, $location) {
-		$scope.isActive = function(route) {
-			return route === $location.path();
-		}
-	}
+  function($scope, $location) {
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    }
+  }
 ]);
 
 moviesApp.controller('HomeCtrl', ['$scope',
-	function($scope) {}
+  function($scope) {}
 ]);
 
 moviesApp.controller('MoviesCtrl', ['$scope', 'dataFactory', '$location',
-	function($scope, dataFactory, $location) {
-		$scope.movies = {};
-		getMovies();
-		function getMovies() {
-			dataFactory.getMovies()
-				.success(function(movies) {
-					$scope.movies = movies;
-				})
-				.error(function(error) {
-					alert('Unable to load movie data');
-				});
-		}
-		$scope.deleteMovie = function(id) {
-			dataFactory.deleteMovie(id)
-				.success(function() {
-					getMovies();
-				})
-				.error(function(error) {
-					alert('Unable to delete movie');
-				});
-		};
-		$scope.updateMovie = function (id) {
-			$location.path('/movie-detail/' + id);
-		};
-		$scope.go = function(path) {
-			$location.path(path);
-		};
-	}
+  function($scope, dataFactory, $location) {
+    $scope.movies = {};
+    getMovies();
+
+    function getMovies() {
+      dataFactory.getMovies()
+        .success(function(movies) {
+          $scope.movies = movies;
+        })
+        .error(function(error) {
+          alert('Unable to load movie data');
+        });
+    }
+    $scope.deleteMovie = function(id) {
+      dataFactory.deleteMovie(id)
+        .success(function() {
+          getMovies();
+        })
+        .error(function(error) {
+          alert('Unable to delete movie');
+        });
+    };
+    $scope.updateMovie = function(id) {
+      $location.path('/movie-detail/' + id);
+    };
+    $scope.go = function(path) {
+      $location.path(path);
+    };
+  }
 ]);
 
 moviesApp.controller('NewMovieCtrl', ['$scope', 'dataFactory', '$location',
-	function($scope, dataFactory, $location) {
-		$scope.createMovie = function() {
-			$scope.$broadcast('show-errors-check-validity');
-			if ($scope.movieForm.$valid) {
-				dataFactory.insertMovie($scope.movie)
-					.success(function() {
-						$location.path('/movie-list');
-					})
-					.error(function(error) {
-						alert('Unable to create movie');
-					});
-			}
-		};
-		$scope.reset = function() {
-			$scope.$broadcast('show-errors-reset');
-			$scope.movie = {
-				title: '',
-				year: '',
-				rated: '',
-				runtime: '',
-				genre: '',
-				director: ''
-			};
-		}
-		$scope.cancel = function(){
-			$location.path('/movie-list');
-		}
-	}
+  function($scope, dataFactory, $location) {
+    $scope.createMovie = function() {
+      $scope.$broadcast('show-errors-check-validity');
+      if ($scope.movieForm.$valid) {
+        dataFactory.insertMovie($scope.movie)
+          .success(function() {
+            $location.path('/movie-list');
+          })
+          .error(function(error) {
+            alert('Unable to create movie');
+          });
+      }
+    };
+    $scope.reset = function() {
+      $scope.$broadcast('show-errors-reset');
+      $scope.movie = {
+        title: '',
+        year: '',
+        rated: '',
+        runtime: '',
+        genre: '',
+        director: ''
+      };
+    }
+    $scope.cancel = function() {
+      $location.path('/movie-list');
+    }
+  }
 ]);
 
 moviesApp.controller('UpdateMovieCtrl', ['$scope', 'dataFactory', '$location', '$routeParams',
-	function($scope, dataFactory, $location, $routeParams) {
-		dataFactory.getMovie($routeParams.id)
-			.success(function(movie) {
-				$scope.movie = movie;
-			})
-			.error(function(error) {
-				alert('Unable to load movie data');
-			});
-		$scope.updateMovie = function () {
-			$scope.$broadcast('show-errors-check-validity');
-			if ($scope.movieForm.$valid) {
-				dataFactory.updateMovie($scope.movie)
-					.success(function(movie) {
-						$location.path('/movie-list');
-					})
-					.error(function(error) {
-						alert('Unable to update movie');
-					});
-			}
-		};
-		$scope.cancel = function(){
-			$location.path('/movie-list');
-		}
-	}
+  function($scope, dataFactory, $location, $routeParams) {
+    dataFactory.getMovie($routeParams.id)
+      .success(function(movie) {
+        $scope.movie = movie;
+      })
+      .error(function(error) {
+        alert('Unable to load movie data');
+      });
+    $scope.updateMovie = function() {
+      $scope.$broadcast('show-errors-check-validity');
+      if ($scope.movieForm.$valid) {
+        dataFactory.updateMovie($scope.movie)
+          .success(function(movie) {
+            $location.path('/movie-list');
+          })
+          .error(function(error) {
+            alert('Unable to update movie');
+          });
+      }
+    };
+    $scope.cancel = function() {
+      $location.path('/movie-list');
+    }
+  }
 ]);
--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,34 @@ var webserver = require('gulp-webserver');
 var opn = require('opn');
 
 var sourcePaths = {
-	styles: ['app/css/**/*.css'],
-	scripts: ['app/js/**/*.js'],
-	partials: ['app/partials/**/*.html']
+  styles: ['app/css/**/*.css'],
+  scripts: ['app/js/**/*.js'],
+  partials: ['app/partials/**/*.html']
 };
 
 var server = {
-	host: 'localhost',
-	port: '3001'
+  host: 'localhost',
+  port: '3001'
 }
 
 gulp.task('webserver', function() {
-	gulp.src('app/.')
-		.pipe(webserver({
-			host: server.host,
-			port: server.port,
-			livereload: true,
-			directoryListing: false
-		}));
+  gulp.src('app/.')
+    .pipe(webserver({
+      host: server.host,
+      port: server.port,
+      livereload: true,
+      directoryListing: false
+    }));
 });
 
 gulp.task('openbrowser', function() {
-	opn('http://' + server.host + ':' + server.port);
+  opn('http://' + server.host + ':' + server.port);
 });
 
 gulp.task('watch', function() {
-	gulp.watch(sourcePaths.styles);
-	gulp.watch(sourcePaths.scripts);
-	gulp.watch(sourcePaths.partials);
+  gulp.watch(sourcePaths.styles);
+  gulp.watch(sourcePaths.scripts);
+  gulp.watch(sourcePaths.partials);
 });
 
 gulp.task('default', ['webserver', 'watch', 'openbrowser']);
